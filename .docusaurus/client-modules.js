c1c8ddab1cfb1f952fac83ef3604d132f27fa091
Markus Ahlstrand <markus@ahlstrand.de>
2022-09-15 11:23:46 +0200

    chore: remove docusaurus files

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/markusahlstrand/Projects/sesamy/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/markusahlstrand/Projects/sesamy/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/markusahlstrand/Projects/sesamy/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/markusahlstrand/Projects/sesamy/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/markusahlstrand/Projects/sesamy/docs/src/css/custom.css'),
-];
